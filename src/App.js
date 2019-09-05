a8bfc229782edaad8cffd3433a302e474e8f76e7
ThelsK <github@thels.nl>
2019-09-05 11:09:49 +0200

    Make localhost request

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react"
+import request from "superagent"
 import "./App.css"
 
 class App extends React.Component {
+
+  componentDidMount() {
+    request("http://localhost:4000/test")
+      .then(console.log)
+      .catch(console.error)
+  }
+
   render() {
     return (
       <div className="App">
